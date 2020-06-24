a13fa6c61eaee4ceb260d4d622726149597b71d6
mari_s <ms.mari.suzuki@gmail.com>
2020-06-25 01:32:27 +0900

    completed 228. Multi Catch Exceptions using | to catch two exceptions

--- Exception/src/academy/learnprogramming/Example.java
+++ Exception/src/academy/learnprogramming/Example.java
@@ -7,24 +7,28 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+
+        } catch(ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
     }
 
     private static int divide() {
         int x, y;
-        try {
+       // try {
             x = getInt();
             y = getInt();
             System.out.println("x is " + x + ", y is " + y);
             return x / y;
-        } catch(NoSuchElementException e) {
-            throw new ArithmeticException("no suitable input");
-        } catch(ArithmeticException e) {
-            throw new ArithmeticException("attempt to divide by zero");
-        }
-
-
+//        } catch(NoSuchElementException e) {
+//            throw new ArithmeticException("no suitable input"); // this should be throw new NoSuchElementException
+//        } catch(ArithmeticException e) {
+//            throw new ArithmeticException("attempt to divide by zero");
+//        }
     }
 
     private static int getInt() {
