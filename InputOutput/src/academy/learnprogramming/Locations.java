fd6551b8a4df2c8de7395a31319dd113390bf5d6
mari_s <ms.mari.suzuki@gmail.com>
2020-06-27 15:22:34 +0900

    230. Writing content - FileWriter class and Finally block added try-catch blocks to file write and close operations in Locations.java

--- InputOutput/src/academy/learnprogramming/Locations.java
+++ InputOutput/src/academy/learnprogramming/Locations.java
@@ -21,6 +21,16 @@ public class Locations implements Map<Integer, Location> {
         } catch(IOException e) {
             System.out.println("In catch block");
             e.printStackTrace();
+        } finally {
+            System.out.println("in finally block");
+            try {
+                if(locFile != null) {
+                    System.out.println("Attempting to close locfile");
+                    locFile.close();
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
