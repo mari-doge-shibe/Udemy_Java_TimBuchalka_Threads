93868ae6d7fe71449b530df692d8f5104e52fc47
mari_s <ms.mari.suzuki@gmail.com>
2020-06-27 15:15:59 +0900

    230. Writing content - FileWriter class and Finally block created locations.txt by running Locations.java

--- InputOutput/src/academy/learnprogramming/Locations.java
+++ InputOutput/src/academy/learnprogramming/Locations.java
@@ -1,5 +1,7 @@
 package academy.learnprogramming;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +9,22 @@ import java.util.Set;
 
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try {
+            locFile = new FileWriter("locations.txt");
+            for(Location location : locations.values()) {
+                locFile.write(location.getLocationID() + "," + location.getDescription());
+            }
+            locFile.close();
+        } catch(IOException e) {
+            System.out.println("In catch block");
+            e.printStackTrace();
+        }
+
+    }
+
 
     static {
         Map<String, Integer> tempExit = new HashMap<>();
