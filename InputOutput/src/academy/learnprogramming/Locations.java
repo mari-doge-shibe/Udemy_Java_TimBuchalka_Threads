33e9178fa5b5a42074d17c0f7039a51be37bf1b3
mari_s <ms.mari.suzuki@gmail.com>
2020-07-02 16:08:11 +0900

    started 234. Load Big Location and Exits Files (added two files from GameData.zip)

--- InputOutput/src/academy/learnprogramming/Locations.java
+++ InputOutput/src/academy/learnprogramming/Locations.java
@@ -42,7 +42,7 @@ public class Locations implements Map<Integer, Location> {
 
         Scanner scanner = null;
         try {
-            scanner = new Scanner(new FileReader("locations.txt"));
+            scanner = new Scanner(new FileReader("locations_big.txt"));
             scanner.useDelimiter(",");
             while(scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
@@ -65,7 +65,7 @@ public class Locations implements Map<Integer, Location> {
 
         // Now read the exits
         try {
-            scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+            scanner = new Scanner(new BufferedReader(new FileReader("directions_big.txt")));
             scanner.useDelimiter(",");
             while(scanner.hasNextLine()){
 //                int loc = scanner.nextInt();
