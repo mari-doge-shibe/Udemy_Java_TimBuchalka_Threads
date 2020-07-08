f87bdeb0d70d4f21038c756cba47727bad78f3c0
mari_s <ms.mari.suzuki@gmail.com>
2020-07-08 20:40:24 +0900

    in progress 241. Create Random Access File created IndexRecord.java

--- AdventureRandom/src/academy/learnprogramming/Locations.java
+++ AdventureRandom/src/academy/learnprogramming/Locations.java
@@ -13,6 +13,11 @@ public class Locations implements Map<Integer, Location> {
 
         try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat", "rwd")){
             rao.writeInt(locations.size());
+            int indexSize = locations.size() * 3 * Integer.BYTES;
+            int locationStart = (int) (indexSize + rao.getFilePointer() + Integer.BYTES);
+            rao.writeInt(locationStart);
+            long indexStart = rao.getFilePointer();
+
             }
         }
 
