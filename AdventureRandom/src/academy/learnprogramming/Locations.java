6fa540766831b84e0a9796ce07895d860d1711e2
mari_s <ms.mari.suzuki@gmail.com>
2020-07-08 20:49:46 +0900

    completed 241. Create Random Access File implemented the try block in Locations.java

--- AdventureRandom/src/academy/learnprogramming/Locations.java
+++ AdventureRandom/src/academy/learnprogramming/Locations.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
+    private static Map<Integer, IndexRecord> index = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
 
@@ -17,6 +18,30 @@ public class Locations implements Map<Integer, Location> {
             int locationStart = (int) (indexSize + rao.getFilePointer() + Integer.BYTES);
             rao.writeInt(locationStart);
             long indexStart = rao.getFilePointer();
+
+            int startPointer = locationStart;
+            rao.seek(startPointer);
+
+            for(Location location : locations.values()) {
+                rao.writeInt(location.getLocationID());
+                rao.writeUTF(location.getDescription());
+                StringBuilder builder = new StringBuilder();
+                for(String direction : location.getExits().keySet()) {
+                    if(!direction.equalsIgnoreCase("Q")) {
+                        builder.append(direction);
+                        builder.append(",");
+                        builder.append(location.getExits().get(direction));
+                        builder.append(",");
+                    }
+                }
+                rao.writeUTF(builder.toString());
+
+                IndexRecord record = new IndexRecord(startPointer, (int) (rao.getFilePointer() - startPointer));
+                index.put(location.getLocationID(), record);
+
+                startPointer = (int) rao.getFilePointer();
+            }
+
 
             }
         }
