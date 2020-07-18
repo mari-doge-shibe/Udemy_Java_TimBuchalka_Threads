63b6ac44cf3e826f4631e13ef6d06f20bdbebba4
mari_s <ms.mari.suzuki@gmail.com>
2020-07-18 23:32:22 +0900

    in progress 246. Reading and Writing with Java NIO implemented File.write to write Line 5

--- JavaNio/src/academy/learnprogramming/Main.java
+++ JavaNio/src/academy/learnprogramming/Main.java
@@ -16,7 +16,7 @@ public class Main {
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
             Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            Files.write(dataPath, "\nLine 4".getBytes("UTF-8"), StandardOpenOption.APPEND);
+            Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
             List<String> lines = Files.readAllLines(dataPath);
             for(String line : lines) {
                 System.out.println(line);
