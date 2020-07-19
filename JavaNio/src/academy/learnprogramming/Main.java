68e02da133701db613c39f02879c3b57dc88f0d4
mari_s <ms.mari.suzuki@gmail.com>
2020-07-19 20:21:07 +0900

    in progress 248. Reading Files with NIO added code to read Hello World! using Java.NIO

--- JavaNio/src/academy/learnprogramming/Main.java
+++ JavaNio/src/academy/learnprogramming/Main.java
@@ -35,15 +35,26 @@ public class Main {
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
 
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            byte[] b = new byte[outputBytes.length];
-            ra.read(b);
-            System.out.println(new String(b));
 
-            long int1 = ra.readInt();
-            long int2 = ra.readInt();
-            System.out.println(int1);
-            System.out.println(int2);
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+            outputBytes[0] = 'a';
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            System.out.println("outputBytes = " + new String(outputBytes));
+
+
+
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            byte[] b = new byte[outputBytes.length];
+//            ra.read(b);
+//            System.out.println(new String(b));
+//
+//            long int1 = ra.readInt();
+//            long int2 = ra.readInt();
+//            System.out.println(int1);
+//            System.out.println(int2);
 
         } catch(IOException e) {
 	        e.printStackTrace();
