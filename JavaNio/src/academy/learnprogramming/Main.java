1161e3a666128d855b699471658d1c8560458f78
mari_s <ms.mari.suzuki@gmail.com>
2020-07-19 16:05:04 +0900

    in progress 248. Reading Files with NIO added RandomAccessFile instance to read Hello World! into the console

--- JavaNio/src/academy/learnprogramming/Main.java
+++ JavaNio/src/academy/learnprogramming/Main.java
@@ -3,6 +3,7 @@ package academy.learnprogramming;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
@@ -16,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
 	    try (FileOutputStream binFile = new FileOutputStream("data.dat");
             FileChannel binChannel = binFile.getChannel()) {
-	        
+
             byte[] outputBytes = "Hello World!".getBytes();
             ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
             int numBytes = binChannel.write(buffer);
@@ -33,6 +34,11 @@ public class Main {
             intBuffer.flip();
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            byte[] b = new byte[outputBytes.length];
+            ra.read(b);
+            System.out.println(new String(b));
 
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
