23153a405cfd90aafb8094f1f35de00f98fc192a
mari_s <ms.mari.suzuki@gmail.com>
2020-11-16 20:47:28 +0900

    in progress 264. Threads added ThreadColor class

--- Threads/src/academy/learnprogramming/Main.java
+++ Threads/src/academy/learnprogramming/Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from the main thread.");
 
-
         Thread anotherThread = new AnotherThread();
         anotherThread.start();
+
+        new Thread() {
+            public void run() {
+                System.out.println("Hello from the anonymous class thread");
+            }
+        }.start();
 
         System.out.println("Hello again from the main thread.");
     }
